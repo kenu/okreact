79e9cf578d8e12fae4991ec14b3c275f9394a111
kenu <kenu.heo@gmail.com>
2021-03-14 17:55:00 +0900

    fetch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './Home.js';
-import About from './About.js';
-import Topics from './Topics.js';
-
+import React, { useState, useEffect } from 'react';
 export default function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    fetch('/data.json')
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data: ', error);
+        setError(error);
+      })
+      .finally((data) => {
+        console.log('data', data);
+        setLoading(false);
+      });
+  }, []);
+  console.log(loading, error, data);
+  if (loading) return 'Loading...';
+  if (error) return 'Error!';
   return (
-    <Router>
-      <div>
-        <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/topics">
-            <Topics />
-          </Route>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </>
   );
 }
-
