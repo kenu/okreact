1eae390316af6dedb1ab8068cb94dd75bfd854ee
kenu <kenu.heo@gmail.com>
2021-03-14 22:39:45 +0900

    react fetch

--- src/About.js
+++ src/About.js
@@ -1,10 +1,40 @@
+import React, { useState, useEffect } from 'react';
 import HeaderCommon from './HeaderCommon.js';
 
 function About() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    fetch('/data.json')
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw response;
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data: ', error);
+        setError(error);
+      })
+      .finally((data) => {
+        console.log('data', data);
+        setLoading(false);
+      });
+  }, []);
+
+  console.log(loading, error, data);
+  if (loading) return 'Loading...';
+  if (error) return 'Error!';
+
   return (
     <>
       <HeaderCommon></HeaderCommon>
       <h2>About 2</h2>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </>
   );
 }
--- src/App.js
+++ src/App.js
@@ -1,34 +1,25 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './Home.js';
+import About from './About.js';
+import Topics from './Topics.js';
+
 export default function App() {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    fetch('/data.json')
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw response;
-      })
-      .then((data) => {
-        setData(data);
-      })
-      .catch((error) => {
-        console.error('Error fetching data: ', error);
-        setError(error);
-      })
-      .finally((data) => {
-        console.log('data', data);
-        setLoading(false);
-      });
-  }, []);
-  console.log(loading, error, data);
-  if (loading) return 'Loading...';
-  if (error) return 'Error!';
   return (
-    <>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/topics">
+            <Topics />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
