dbc61ef69d19cb9479e869ea9477677957e10676
kenu <kenu.heo@gmail.com>
2021-03-15 21:28:09 +0900

    use-http

--- src/About.js
+++ src/About.js
@@ -1,33 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import useFetch from 'use-http';
 import HeaderCommon from './HeaderCommon.js';
 
 function About() {
-  const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    fetch('/data.json')
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw response;
-      })
-      .then((data) => {
-        setData(data);
-      })
-      .catch((error) => {
-        console.error('Error fetching data: ', error);
-        setError(error);
-      })
-      .finally((data) => {
-        console.log('data', data);
-        setLoading(false);
-      });
-  }, []);
+  const { isLoading, error, data = [] } = useFetch('/data.json', {}, []);
 
-  console.log(loading, error, data);
-  if (loading) return 'Loading...';
+  console.log(isLoading, error, data);
+  if (isLoading) return 'Loading...';
   if (error) return 'Error!';
 
   return (
