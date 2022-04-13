2f536fd761b1e7e8e10f842d558bdd30ce43367e
Ted <skcchwang@gmail.com>
2022-04-13 14:57:50 +0900

    ✨ Router #4

--- secondreact/src/components/Header.jsx
+++ secondreact/src/components/Header.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+
 function Header() {
     return (
         <header className="header">
             <h2>Header</h2>
+            <nav>
+                <a href="/"> HOME OLD </a> |
+                <Link to="/">Home</Link> |
+                <Link to="/about">About</Link>
+            </nav>
         </header>
     );
 }
--- secondreact/src/main.jsx
+++ secondreact/src/main.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './App'
 import Header from './components/Header'
 
 ReactDOM.render(
   <React.StrictMode>
+    
+    <BrowserRouter>
     <Header />
-    <App />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              <h1>Hello World</h1>
+            </div>
+          }
+        ></Route>
+        <Route
+          path="/about"
+          element={
+            <div>
+              <h1>About React</h1>
+            </div>
+          }
+        ></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 )
