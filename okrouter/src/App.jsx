be072647cda1fd3784e4e498a8de376e5731967a
kenu <kenu.heo@gmail.com>
2022-04-27 16:47:00 +0900

    netlify

--- okrouter/src/App.jsx
+++ okrouter/src/App.jsx
@@ -1,9 +1,15 @@
-function App() {
+import Comp from './components/Comp.jsx';
+
+function App(props) {
+  const list = ['hello1', 'hello2', 'hello3'];
   return (
     <div className="App">
       <h1>Main</h1>
+      {list.map((item, index) => (
+        <Comp value={item} key={index} />
+      ))}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ okrouter/src/components/Comp.jsx
@@ -0,0 +1,7 @@
+function Comp(props) {
+  return (
+    <p>{ props.value }</p>
+  );
+}
+
+export default Comp;
